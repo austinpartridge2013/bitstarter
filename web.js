35d6c1adba04f1febb11d7ad7ac83f38cd96bbb3
Ubuntu <ubuntu@ip-172-31-27-58.us-west-2.compute.internal>
2013-07-15 01:03:01 +0000

    Updating by adding buffer

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
+var buffer = new Buffer(16);
 
 app.get('/', function(request, response) {
   var contents = fs.readFileSync('index.html');
-  response.send(contents);
+  response.send(buffer.write(contents, "utf-8"));
 });
 
 var port = process.env.PORT || 5000;
